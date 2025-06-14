54a986de1baf1bf94ce5dd82547e600a0b0b8811
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-14 16:16:41 +0530

    PROJECT COMPLETED

--- Project_03_Guess-My-Number/script.js
+++ Project_03_Guess-My-Number/script.js
@@ -11,40 +11,30 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 
-
-// Computing the Secret number
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-// Defining the score and high score
 let score = 20;
 let highScore = 0;
 
-// Initialising the check button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  // When there is no Input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
 
-    // When there is correct Input
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
 
-    // Setting the highScore
     if (highScore < score) {
       highScore = score;
     }
     document.querySelector('.highscore').textContent = highScore;
 
-    // Changing the color for the background when the answer is correct
     document.querySelector('body').style.backgroundColor = '#60b347';
 
-    // Changing the size of the number when the correct answer
     document.querySelector('.number').style.width = '30rem';
 
-    // when the guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High !';
@@ -55,7 +45,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
 
-    // When the guess is low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low !';
@@ -68,7 +57,6 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 }); 
 
-// Setting up the again button
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   document.querySelector('.score').textContent = score;
@@ -80,4 +68,4 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
 });
 
-//------------------------------------------------------------------------------------------------------//
+//------------------------------------------------------------COMPLETED---------------------------------------------------------------//
